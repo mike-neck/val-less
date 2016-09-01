057e67f7fda2d6b55907c2947abc650f84be42df
mike_neck <jkrt3333@gmail.com>
2016-09-02 07:32:20 +0900

    add choice

---
+++ src/main/kotlin/util/basic/Choice.kt
@@ -0,0 +1,32 @@
+/*
+ * Copyright 2016 Shinya Mochida
+ * 
+ * Licensed under the Apache License,Version2.0(the"License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing,software
+ * Distributed under the License is distributed on an"AS IS"BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package util.basic
+
+import java.util.*
+
+sealed class Choice<out F, out S> {
+
+    abstract val first: F
+    abstract val second: S
+
+    class First<out F, out S>(override val first: F) : Choice<F, S>() {
+        override val second: S get() = throw NoSuchElementException("This is first.")
+    }
+
+    class Second<out F, out S>(override val second: S) : Choice<F, S>() {
+        override val first: F get() = throw NoSuchElementException("This is second.")
+    }
+}
