02028ce716565ce5b536474dfe59d44485eef392
mike_neck <jkrt3333@gmail.com>
2016-09-07 11:19:58 +0900

    add Either type

---
+++ src/main/kotlin/valless/type/data/Either.kt
@@ -0,0 +1,27 @@
+/*
+ * Copyright 2016 Shinya Mochida
+ * 
+ * Licensed under the Apache License,Version2.0(the"License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing,software
+ * Distributed under the License is distributed on an"AS IS"BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package valless.type.data
+
+import valless.type._2
+
+sealed class Either<L, R> : _2<Either.Companion, L, R> {
+
+    class Left<L, R>(val left: L) : Either<L, R>()
+
+    class Right<L, R>(val right: R) : Either<L, R>()
+
+    companion object
+}
